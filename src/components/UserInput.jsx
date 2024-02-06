fe9fb2376b164eb18e2ce14ca2050d27a6e4073f
Jeff Ripke <jripke74@gmail.com>
2024-02-06 10:37:05 -0800

    add UserInput state

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,22 +1,66 @@
+import { useState } from 'react';
+
 export default function UserInput() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expextedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <label>Initial Investment</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(event) =>
+              handleChange('initialInvestment', event.target.value)
+            }
+          />
         </p>
         <p>
           <label>Annual Investment</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(event) =>
+              handleChange('annualInvestment', event.target.value)
+            }
+          />
         </p>
         <p>
           <label>Expected Return</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={userInput.expextedReturn}
+            onChange={(event) =>
+              handleChange('expextedReturn', event.target.value)
+            }
+          />
         </p>
         <p>
           <label>Duration</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(event) => handleChange('duration', event.target.value)}
+          />
         </p>
       </div>
     </section>
